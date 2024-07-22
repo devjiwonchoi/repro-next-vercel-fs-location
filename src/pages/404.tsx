7cb175fc921418463b6412064f0d8a57516baad3
devjiwonchoi <devjiwonchoi@gmail.com>
2024-07-23 03:59:23 +0900

    move to public

--- src/pages/404.tsx
+++
@@ -1,7 +0,0 @@
-import { readJSON } from '@/lib'
-
-export default async function FourOFour() {
-  const fooOrbar = Math.random() > 0.5 ? 'foo' : 'bar'
-  const json = await readJSON(fooOrbar)
-  return <p>{json.value}</p>
-}
